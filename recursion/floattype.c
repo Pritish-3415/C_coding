1af10248dee1aebab5f95abf8f9f87f78d1a436a
Pritish_3415 <kpritish15@gmail.com>
2025-12-02 21:11:18 +0530

    first commit

--- recursion/floattype.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("hello\n");   // \n moves to next line
-    int x = 5;
-    int y = 6;
-    int z;
-    printf("%d\n", z = x + y);  // \n again for clean output
-    return 0;
-}
--- recursion/pattern_printing.c/firstcode.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void){
-    char a,b,c;
-    // Leading space in " %c" skips any whitespace/newlines
-    scanf(" %c %c %c", &a, &b, &c);
-    printf("%c %c %c\n", a, b, c);
-    return 0;
-}
